f7456788e1b73b31199d0094df4f3872536521a5
Ishtiaq <ishtiaq.wasi@gmail.com>
2026-01-19 21:13:51 +1100

    added footer, fixed nav links

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,10 +1,76 @@
 import React from 'react';
+import Logo from '../Logo';
+import NavLink from '../buttons/NavLink';
+import Link from 'next/link';
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+
 
 const Navbar = () => {
+
+    const links = <>
+    <li>
+       <NavLink href={'/'}>Home</NavLink>
+    </li>
+    <li>
+       <NavLink href={'/packages'}>Packages</NavLink>
+    </li>
+    <li>
+       <NavLink href={'/blogs'}>Blogs</NavLink>
+    </li>
+    <li>
+       <NavLink href={'/contact'}>Contact</NavLink>
+    </li>
+  
+    </>
     return (
-        <div>
-            Navbar
+      <div>
+        <div className="navbar ">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />{" "}
+                </svg>
+              </div>
+              <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                {links}
+              </ul>
+            </div>
+            <Logo />
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{links}</ul>
+          </div>
+          <div className="navbar-end space-x-4">
+            <Link href={"/cart"} className="btn btn-primary font-bold ">
+              <MdOutlineShoppingCart />
+            </Link>
+            <Link href={"/login"}>
+              <button className="btn btn-primary btn-outline">Login</button>
+            </Link>
+          </div>
         </div>
+      </div>
     );
 };
 
